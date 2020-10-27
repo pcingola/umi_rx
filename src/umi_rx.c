2b0bcc4aa589524cec27f7ff0a80f364d0e7cd94
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2020-10-27 05:56:18 -0400

    Project updated

--- src/umi_rx.c
+++ src/umi_rx.c
@@ -32,13 +32,13 @@ int main(int argc, char **argv) {
     sam_hdr_t *header = sam_hdr_read(in);
     if (header == NULL) {
         fprintf(stderr, "Couldn't read header for \"%s\"\n", filein);
-        return 1;
+        exit(1);
     }
 
     // Write header
     if (sam_hdr_write(out, header) < 0) {
         fprintf(stderr, "Error writing output header.\n");
-        return 1;
+        exit(1);
     }
 
     bam1_t *aln = bam_init1();
@@ -48,42 +48,47 @@ int main(int argc, char **argv) {
         int32_t pos = aln->core.pos + 1;
         char *chr = header->target_name[aln->core.tid];
 
+        // Show every 1M reads
         if( read_num % 1000000 == 0 ) {
-            printf("read_num=%ld, chr=%s, pos=%d, read_name=%s\n", read_num, chr, pos, read_name);
+            printf("read_number=%ld, chr=%s, pos=%d, read_name=%s\n", read_num, chr, pos, read_name);
         }
 
         // Find UMI part
         char *rx = strrchr(read_name, ':');
         if (!rx) {
-            fprintf(stderr, "Error: Could not find UMI from read name, read_num=%ld, chr=%s, pos=%d, read_name=%s\n", read_num, chr, pos, read_name);
-            return 1;
+            fprintf(stderr, "Error: Could not find UMI from read name, read_number=%ld, chr=%s, pos=%d, read_name=%s\n", read_num, chr, pos, read_name);
+            exit(1);
         }
 
         // Add UMI to 'RX' tag
         if (bam_aux_append(aln, "RX", 'Z', strlen(rx) + 1, (uint8_t *) rx) < 0) {
             fprintf(stderr, "Error updating RX tag");
-            return 1;
+            exit(1);
         }
 
         // Write alignment to output
         if (sam_write1(out, header, aln) < 0) {
             fprintf(stderr, "Error writing output alignment, read number %ld\n", read_num);
-            return 1;
+            exit(1);
         }
     }
 
-    if (aln) bam_destroy1(aln);
-    if (header) sam_hdr_destroy(header);
+    printf("Finished: %ld reads processed\n", read_num);
+
+    // Close files
+    if (hts_close(out) < 0) {
+        fprintf(stderr, "Error closing \"%s\"\n", fileout);
+        exit(1);
+    }
 
     if (hts_close(in) < 0) {
         fprintf(stderr, "Error closing \"%s\"\n", filein);
         exit(1);
     }
 
-    if (hts_close(out) < 0) {
-        fprintf(stderr, "Error closing \"%s\"\n", fileout);
-        exit(1);
-    }
+    // Free memory
+    bam_destroy1(aln);
+    sam_hdr_destroy(header);
 
     return 0;
 }
